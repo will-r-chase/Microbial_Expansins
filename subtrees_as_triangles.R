fcb489b0a70ee13613b8c689faf4ce557b25e0cf
William Chase <wchase14@gmail.com>
2018-07-10 18:29:09 -0400

    trying to reduce tree size with collapse

--- subtrees_as_triangles.R
+++ subtrees_as_triangles.R
@@ -5,7 +5,8 @@ library(ggplot2)
 library(dplyr)
 library(tidytree)
 library(magrittr)
-
+library(emojifont)
+library(grDevices)
 
 ##subtrees as triangles functions##
 ###########################################################################
@@ -120,5 +121,72 @@ triangles_plot <-
     strip.background = element_blank()
   )
 
+#test tree w/ small triangles
+collapse_tree<-
+  ggtree(group_test, aes(color=group)) + 
+  geom_cladelabel(node = 903, label = "Prokaryotes", color = "black", barsize = 2, align = T, fontsize = 5, offset = 1) +
+  geom_cladelabel(node = 610, label = "Eukaryotes", color = "black", barsize = 2, align = T, fontsize = 5, offset = 1) +
+  xlim(0, 8) +
+  scale_color_manual(values = c("darkgrey", "red"))
+
+for(i in 1:length(nodes_to_collapse)){
+  collapse_tree <- collapse(collapse_tree, node = nodes_to_collapse[i]) + 
+    geom_point2(aes(subset = (node == nodes_to_collapse[i])), size = 3, shape = 23, fill = "blue")
+}
+
+
+##### NOTE TO SELF, latest solution, use clade_label with collapse to label collapsed clades then edit in inkscape
+##### https://groups.google.com/forum/?utm_medium=email&utm_source=footer#!msg/bioc-ggtree/0H1PCJPlI1Q/aJy9qY50DQAJ
+collapse_tree
+
+for(i in 1:length(nodes_to_collapse)){
+  collapse_tree <- collapse_tree + 
+    geom_point2(aes(subset = (node == nodes_to_collapse[i])), size = 3, shape = 23, fill = "blue")
+}
+
+test_collapse<-test_tree %>% collapse(node = 923)
+test_collapse<-test_collapse+geom_text2(aes(subset=(node == 923)), size=10, label=intToUtf8(9668), hjust=-0.01, vjust=0.4)
+cairo_pdf(filename="cairo_test.pdf", width=20, height=20)
+test_collapse
+dev.off()
+
+
+#scale_plot to triangles
+test_tree<-
+  ggtree(group_test, aes(color=group)) %>% 
+  scaleClade(923, scale=0.1) + 
+  geom_cladelabel(node = 903, label = "Prokaryotes", color = "black", barsize = 2, align = T, fontsize = 5, offset = 1) +
+  geom_cladelabel(node = 610, label = "Eukaryotes", color = "black", barsize = 2, align = T, fontsize = 5, offset = 1) +
+  xlim(0, 8) +
+  scale_color_manual(values = c("darkgrey", "red"))
+test_tree
+scaled_tree<-as.phylo(test_tree)
+group_scaled<-groupOTU(scaled_tree, .node=c("Ralstonia-syzygii", "Ralstonia-solanacearum", "Erwinia-tracheiphila", "Pantoea-stewartii", "Lonsdalea-quercina", "Pectobacterium-atrosepticum", "Pectobacterium-wasabiae", "Pectobacterium-parmentieri", "Pectobacterium-betavasculorum", "Pectobacterium-carotovorum", "Dickeya-zeae", "Dickeya-dadantii", "Dickeya-dianthicola", "Dickeya-chrysanthemi", "Dickeya-solani", "Cedecea-neteri", "Sphaerotilus-natas", "Janthinobacterium-sp", "Methylibium-sp", "Acidovorax-radicis", "Leptothrix-cholodnii", "Polyangium-brachysporum", "Vitrella-brassicaformis", "Sorangium-cellulosum", "Neocallimastix-californiae", "Anaeromyces-robustus", "Piromyces-finnis", "Allomyces-macrogynus", "Hamadaea-tsunoensis", "Streptomyces-acidiscabies", "Uliginosibacterium-gangwonense", "Haloferula-sp", "Physarum-polycephalum", "Acanthamoeba-castellanii", "Trichoderma-harzianum", "Trichoderma-virens", "Trichoderma-pseudokoningii", "Trichoderma-reesei", "Trichoderma-asperellum", "Trichoderma-atroviride", "Talaromyces-stipitatus2", "Penicillium-decumbens", "Penicillium-oxalicum", "Penicillium-brasilianum2", "Talaromyces-cellulolyticus3", "Talaromyces-marneffei", "Talaromyces-verruculosus2", "Thalassiosira-oceanica"))
+
+
+collapsed_tree_df <- group_scaled %>% 
+  remove_collapsed_nodes(nodes = nodes_to_collapse)
+
+#get coords for triangles
+triangles_df <- group_scaled %>%
+  get_triangle_coordinates(nodes_to_collapse, mode = "min")
+
+#plot ggtree with collapsed nodes as triangles
+triangles_plot <- 
+  ggtree(collapsed_tree_df, aes(color=group)) +
+  geom_cladelabel(node = 903, label = "Prokaryotes", color = "black", barsize = 2, align = T, fontsize = 5, offset = 1) +
+  geom_cladelabel(node = 610, label = "Eukaryotes", color = "black", barsize = 2, align = T, fontsize = 5, offset = 1) +
+  xlim(0, 8) +
+  scale_color_manual(values = c("darkgrey", "red")) +
+  geom_polygon(
+    data = triangles_df,
+    mapping = aes(group = node_collapsed),
+    color = "#333333",
+    fill = "steelblue"
+  ) +
+  theme(
+    strip.background = element_blank()
+  )
+triangles_plot
 
 
