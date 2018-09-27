8f8f1d71685860f5333fac8e45392f413a24fcdd
William Chase <wchase14@gmail.com>
2018-09-26 21:21:06 -0400

    update poster ecology plots

--- poster_ecology_plots.R
+++ poster_ecology_plots.R
@@ -43,6 +43,7 @@ data_filtered <- data %>%
 colnames(data_filtered) <- c("organism", "group", "group2", "plant_path", "plant_associated", "ecology", "kingdom")
 data_filtered$plant_associated[which(data_filtered$plant_associated=="No" & data_filtered$plant_path=="Yes")] <- "Yes"
 data_filtered$ecology[which(data_filtered$ecology=="Extremophile; Commensal")] <- "Extremophile"
+data_filtered$ecology[data_filtered$organism=="Mastigocoleus-testarum"] <- "Marine"
 
 #define colors for taxonomy coloring
 colors <- c("#016c59", "#33a02c", "#091b77", "#cece3e", "#8c510a", "#c51b7d", "#252525", "#ff7f00", "#4b93c3", "#8861d4")
@@ -75,6 +76,13 @@ ecology_plots <- lapply(ecology_split, function(x){
 }
 )  
 
+ecology_plots[[1]]
+ggsave("bacteria_ecology.tiff", dpi = 300)
+ecology_plots[[2]]
+ggsave("fungi_ecology.tiff", dpi = 300)
+ecology_plots[[3]]
+ggsave("protist_ecology.tiff", dpi = 300)
+
 #make plant association barplots 
 plant_associated_counts <- data_filtered %>% 
   filter(plant_associated != "UnkNown") %>%
@@ -90,7 +98,15 @@ plant_associated_plots <- lapply(plant_associated_split, function(x){
     scale_fill_manual(values = c("white", "springgreen4")) +
     geom_col(color = "black", size = 0.5) + 
     theme_minimal() +
-    theme(axis.text = element_blank(), axis.title = element_blank()) +
+    theme(axis.text = element_blank(), axis.title = element_blank(), legend.position = "none") +
     geom_text(position = position_stack(vjust = 0.5), size = 6)
 }
 )
+
+plant_associated_plots[[1]]
+ggsave("bacteria_associated.tiff", dpi = 300, units = "mm", width = 70, height = 170)
+plant_associated_plots[[2]]
+ggsave("fungi_associated.tiff", dpi = 300, units = "mm", width = 70, height = 170)
+plant_associated_plots[[3]]
+ggsave("protist_associated.tiff", dpi = 300, units = "mm", width = 70, height = 170)
+
